c04b0ecb5a8c0854dcf01032af0a84618f1253c0
Hung-Tan-Tran <HTT15@my.fsu.edu>
2018-12-06 22:57:39 -0500

    Set theme, added floating action button and recycler view

--- app/src/main/java/edu/fsu/cs/mobile/snoozemenot/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/snoozemenot/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.fsu.cs.mobile.snoozemenot;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button Create;
+    FloatingActionButton createAlarm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Create = findViewById(R.id.create_button);
-        Create.setOnClickListener(new View.OnClickListener(){
+        createAlarm = findViewById(R.id.create_alarm);
+        createAlarm.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View v) {
             Intent myIntent = new Intent(MainActivity.this,CreateUpdateAlarm.class);
