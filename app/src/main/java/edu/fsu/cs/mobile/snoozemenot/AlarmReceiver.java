78e0c12f3daad3e6790a99c98ea2bf537b2b4985
gradonstone <gradonstone@gmail.com>
2018-12-12 20:42:18 -0500

    Gets time data from create and sends back to main activity, alarm manager should be working too!

---
+++ app/src/main/java/edu/fsu/cs/mobile/snoozemenot/AlarmReceiver.java
@@ -0,0 +1,27 @@
+package edu.fsu.cs.mobile.snoozemenot;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "WAKE UP!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        // gonna have to do stuff here to shutt the alarm up
+
+    }
+}
